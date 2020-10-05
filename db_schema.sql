670c57198e9540146f63a8fa00256d5891a5feed
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-10-05 16:25:54 +0300

    edit : reformat of file

--- db_schema.sql
+++ db_schema.sql
@@ -11,25 +11,19 @@ CREATE TABLE IF NOT EXISTS `cook` (
     id SERIAL,
     userID BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (userID)
-        REFERENCES users (id)
-        ON UPDATE CASCADE
+    FOREIGN KEY (userID) REFERENCES users (id) ON UPDATE CASCADE
 );
 CREATE TABLE IF NOT EXISTS `waiter` (
     `id` SERIAL,
     `userID` BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`userID`)
-        REFERENCES `users` (`id`)
-        ON UPDATE CASCADE
+    FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON UPDATE CASCADE
 );
 CREATE TABLE IF NOT EXISTS `admin` (
     `id` SERIAL,
     `userID` BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`userID`)
-        REFERENCES `users` (`id`)
-        ON UPDATE CASCADE
+    FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON UPDATE CASCADE
 );
 CREATE TABLE IF NOT EXISTS `orders` (
     `id` SERIAL,
@@ -37,28 +31,21 @@ CREATE TABLE IF NOT EXISTS `orders` (
     `tableID` INT,
     `orderStatus` TINYINT,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`waiterID`)
-        REFERENCES `waiter` (`id`)
-        ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (`waiterID`) REFERENCES `waiter` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE IF NOT EXISTS dish (
     `id` SERIAL PRIMARY KEY,
     `dishName` BIGINT,
     `dishCost` INT,
     `dishAvailibility` TINYINT
-); 
+);
 CREATE TABLE IF NOT EXISTS `orderItems` (
     `id` SERIAL,
     `dishID` BIGINT UNSIGNED NOT NULL,
     `orderID` BIGINT UNSIGNED NOT NULL,
     `quantity` TINYINT,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`dishID`)
-        REFERENCES `dish` (`id`)
-        ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (`orderID`)
-        REFERENCES `orders` (`id`)
-        ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (`dishID`) REFERENCES `dish` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`orderID`) REFERENCES `orders` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
 -- show tables from restaurant;
