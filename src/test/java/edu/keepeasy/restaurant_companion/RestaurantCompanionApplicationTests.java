98fdfe422bd443d881ee12044aa5bc2fb67264e8
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-11 16:35:35 +0300

    add: template spring project

---
+++ src/test/java/edu/keepeasy/restaurant_companion/RestaurantCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.keepeasy.restaurant_companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
