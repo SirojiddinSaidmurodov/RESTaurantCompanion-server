1596547697222a1b72603ec244dfff96f655ab3e
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 20:35:36 +0300

    edit: changed create, update and delete methods signature
    Took 10 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/repository/Repository.java
+++ src/main/java/edu/keepeasy/restaurant_companion/repository/Repository.java
@@ -3,13 +3,13 @@ package edu.keepeasy.restaurant_companion.repository;
 import edu.keepeasy.restaurant_companion.domain.Entity;
 
 public interface Repository<T extends Entity> {
-    void create(T entity);
+    T create(T entity);
 
     T[] readAll();
 
     T read(long id);
 
-    void update(long id, T entity);
+    T update(long id, T entity);
 
-    void delete(T entity);
+    T delete(T entity);
 }
