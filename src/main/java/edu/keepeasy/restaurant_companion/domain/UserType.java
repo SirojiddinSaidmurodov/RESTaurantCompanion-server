a957320c0f38ac07b29542124dc57f1a73be4111
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 21:41:55 +0300

    edit: integer values
    Took 27 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/domain/UserType.java
+++ src/main/java/edu/keepeasy/restaurant_companion/domain/UserType.java
@@ -1,8 +1,26 @@
 package edu.keepeasy.restaurant_companion.domain;
 
 public enum UserType {
-    ADMIN,
-    COOK,
-    WAITER
+    UNDEFINED(0),
+    ADMIN(1),
+    COOK(2),
+    WAITER(3);
+    private final int value;
 
+    UserType(int value) {
+        this.value = value;
+    }
+
+    public static UserType fromInteger(int value) {
+        return switch (value) {
+            case 1 -> ADMIN;
+            case 2 -> COOK;
+            case 3 -> WAITER;
+            default -> UNDEFINED;
+        };
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
