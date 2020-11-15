1430565413aba85b99482ae39573f6703d802ee5
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 21:52:25 +0300

    edit: added create method
    Took 10 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/repository/UserRepo.java
+++ src/main/java/edu/keepeasy/restaurant_companion/repository/UserRepo.java
@@ -1,12 +1,26 @@
 package edu.keepeasy.restaurant_companion.repository;
 
 import edu.keepeasy.restaurant_companion.domain.User;
+import edu.keepeasy.restaurant_companion.domain.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import java.sql.Types;
+
+@org.springframework.stereotype.Repository
 public class UserRepo implements Repository<User> {
+    private static final String insertQuery = "INSERT INTO user(name, login, passwordHash, userType) VALUE (?,?,?,?)";
+
+    @Autowired
+    JdbcOperations jdbcOperations;
 
     @Override
     public User create(User entity) {
-        return null;
+        Object[] args = new Object[]{entity.getName(), entity.getLogin(), entity.getPassword(), entity.getUserType().getValue()};
+        int[] argTypes = new int[]{Types.CHAR, Types.CHAR, Types.CHAR, Types.INTEGER};
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet(insertQuery);
+        return getResult(rowSet);
     }
 
     @Override
@@ -27,5 +41,18 @@ public class UserRepo implements Repository<User> {
     @Override
     public User delete(User entity) {
         return null;
+    }
+
+    private User getResult(SqlRowSet rowSet) {
+        if (rowSet.next()) {
+            return new User(
+                    rowSet.getLong("id"),
+                    rowSet.getString("name"),
+                    rowSet.getString("login"),
+                    rowSet.getString("password"),
+                    UserType.fromInteger(rowSet.getInt("userType")));
+        } else {
+            return null;
+        }
     }
 }
