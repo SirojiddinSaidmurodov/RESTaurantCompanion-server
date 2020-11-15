f053da4335396929e67d3796370a4bedeb65de3f
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 16:11:56 +0300

    edit: changed database architecture
    Took 18 minutes

--- db_schema.sql
+++ db_schema.sql
@@ -1,36 +1,35 @@
 drop database if exists restaurant;
 create database restaurant;
 use restaurant;
-CREATE TABLE IF NOT EXISTS `user` (
-    id SERIAL PRIMARY KEY,
-    `name` CHAR(40),
-    `login` CHAR(40),
-    `passwordHash` CHAR(64)
-) comment = "Basic user entity";
-CREATE TABLE IF NOT EXISTS `waiter` (
-    `id` SERIAL,
-    `userID` BIGINT UNSIGNED NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`userID`) REFERENCES `user` (`id`) ON UPDATE CASCADE
-)comment = "Users that can add orders";
-CREATE TABLE IF NOT EXISTS `orders` (
-    `id` SERIAL,
-    `waiterID` BIGINT UNSIGNED NOT NULL,
-    `tableID` INT,
+CREATE TABLE IF NOT EXISTS `user`
+(
+    id             SERIAL PRIMARY KEY,
+    `name`         CHAR(40),
+    `login`        CHAR(40),
+    `passwordHash` CHAR(64),
+    `userType`     ENUM ('ADMIN', 'COOK', 'WAITER')
+) comment = 'Basic user entity';
+CREATE TABLE IF NOT EXISTS `orders`
+(
+    `id`          SERIAL,
+    `waiterID`    BIGINT UNSIGNED NOT NULL,
+    `tableID`     INT,
     `orderStatus` BOOLEAN,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`waiterID`) REFERENCES `waiter` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (`waiterID`) REFERENCES `user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE TABLE IF NOT EXISTS meal (
-    `id` SERIAL PRIMARY KEY,
-    `mealName` CHAR(50),
-    `mealCost` INT,
+CREATE TABLE IF NOT EXISTS meal
+(
+    `id`            SERIAL PRIMARY KEY,
+    `mealName`      CHAR(50),
+    `mealCost`      INT,
     `mealAvailable` BOOLEAN
 );
-CREATE TABLE IF NOT EXISTS `orderItems` (
-    `id` SERIAL,
-    `mealID` BIGINT UNSIGNED NOT NULL,
-    `orderID` BIGINT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `orderItems`
+(
+    `id`       SERIAL,
+    `mealID`   BIGINT UNSIGNED NOT NULL,
+    `orderID`  BIGINT UNSIGNED NOT NULL,
     `quantity` TINYINT,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`mealID`) REFERENCES `meal` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
