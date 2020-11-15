c1fdb0f398057c8715377411698db3f5ce073b75
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 23:00:49 +0300

    refactor: moved UserType into the User class
    Took 5 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/domain/User.java
+++ src/main/java/edu/keepeasy/restaurant_companion/domain/User.java
@@ -45,4 +45,29 @@ public class User extends Entity{
     public void setUserType(UserType userType) {
         this.userType = userType;
     }
+
+    public enum UserType {
+        UNDEFINED(0),
+        ADMIN(1),
+        COOK(2),
+        WAITER(3);
+        private final int value;
+
+        UserType(int value) {
+            this.value = value;
+        }
+
+        public static UserType fromInteger(int value) {
+            return switch (value) {
+                case 1 -> ADMIN;
+                case 2 -> COOK;
+                case 3 -> WAITER;
+                default -> UNDEFINED;
+            };
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
 }
