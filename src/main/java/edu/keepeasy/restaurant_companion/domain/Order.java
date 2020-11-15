3fa7388f788a7dce747a01a8b1662cf57787600d
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 23:03:19 +0300

    edit: removed orderItems field
    Took 3 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/domain/Order.java
+++ src/main/java/edu/keepeasy/restaurant_companion/domain/Order.java
@@ -4,22 +4,12 @@ public class Order extends Entity {
     private long waiterID;
     private int tableID;
     private boolean orderStatus;
-    private OrderItem[] orderItems;
 
-    public Order(long id, long waiterID, int tableID, boolean orderStatus, OrderItem[] orderItems) {
+    public Order(long id, long waiterID, int tableID, boolean orderStatus) {
         super(id);
         this.waiterID = waiterID;
         this.tableID = tableID;
         this.orderStatus = orderStatus;
-        this.orderItems = orderItems;
-    }
-
-    public OrderItem[] getMeals() {
-        return orderItems;
-    }
-
-    public void setMeals(OrderItem[] orderItems) {
-        this.orderItems = orderItems;
     }
 
     public long getWaiterID() {
