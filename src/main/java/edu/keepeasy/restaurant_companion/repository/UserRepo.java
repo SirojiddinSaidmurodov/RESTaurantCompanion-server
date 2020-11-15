726419dc4402dce1ba0626c815fb2b517fe0de88
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 22:28:10 +0300

    edit: added create, read, readAll, update, delete methods
    Took 26 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/repository/UserRepo.java
+++ src/main/java/edu/keepeasy/restaurant_companion/repository/UserRepo.java
@@ -7,40 +7,81 @@ import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.sql.Types;
+import java.util.ArrayList;
 
 @org.springframework.stereotype.Repository
 public class UserRepo implements Repository<User> {
     private static final String insertQuery = "INSERT INTO user(name, login, passwordHash, userType) VALUE (?,?,?,?)";
-
+    private static final String selectAllQuery = "SELECT id, name, login, passwordHash, userType FROM user";
+    private static final String selectByID = "SELECT id, name, login, passwordHash, userType FROM user where id=?";
+    private static final String deleteQuery = "DELETE FROM user where id=?";
     @Autowired
     JdbcOperations jdbcOperations;
 
     @Override
     public User create(User entity) {
-        Object[] args = new Object[]{entity.getName(), entity.getLogin(), entity.getPassword(), entity.getUserType().getValue()};
-        int[] argTypes = new int[]{Types.CHAR, Types.CHAR, Types.CHAR, Types.INTEGER};
-        SqlRowSet rowSet = jdbcOperations.queryForRowSet(insertQuery);
+        Object[] args = new Object[]{
+                entity.getName(),
+                entity.getLogin(),
+                entity.getPassword(),
+                entity.getUserType().getValue()};
+        int[] argTypes = new int[]{
+                Types.CHAR,
+                Types.CHAR,
+                Types.CHAR,
+                Types.INTEGER};
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet(insertQuery, args, argTypes);
         return getResult(rowSet);
     }
 
     @Override
     public User[] readAll() {
-        return new User[0];
+        ArrayList<User> users = new ArrayList<>();
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet(selectAllQuery);
+        User user = getResult(rowSet);
+        while (user != null) {
+            users.add(user);
+            user = getResult(rowSet);
+        }
+        return users.toArray(new User[0]);
     }
 
     @Override
     public User read(long id) {
-        return null;
+        Object[] args = new Object[]{id};
+        int[] argTypes = new int[]{Types.BIGINT};
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet(selectByID, args, argTypes);
+        return getResult(rowSet);
     }
 
     @Override
     public User update(long id, User entity) {
-        return null;
+        Object[] args = new Object[]{
+                entity.getId(),
+                entity.getName(),
+                entity.getLogin(),
+                entity.getPassword(),
+                entity.getUserType().getValue(),
+                id};
+        int[] argTypes = new int[]{
+                Types.BIGINT,
+                Types.CHAR,
+                Types.CHAR,
+                Types.CHAR,
+                Types.INTEGER,
+                Types.INTEGER};
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet(selectByID, args, argTypes);
+        return getResult(rowSet);
     }
 
     @Override
     public User delete(User entity) {
-        return null;
+        Object[] args = new Object[]{
+                entity.getId()};
+        int[] argTypes = new int[]{
+                Types.BIGINT};
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet(deleteQuery);
+        return getResult(rowSet);
     }
 
     private User getResult(SqlRowSet rowSet) {
