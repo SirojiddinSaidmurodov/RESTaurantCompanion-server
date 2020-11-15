5723d0b141e25a03ea9642f1dcebbbf88cb132c4
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 17:36:03 +0300

    edit: added insert and select all methods
    Took 1 hour 9 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/repository/MealRepo.java
+++ src/main/java/edu/keepeasy/restaurant_companion/repository/MealRepo.java
@@ -1,6 +1,18 @@
 package edu.keepeasy.restaurant_companion.repository;
 
 import edu.keepeasy.restaurant_companion.domain.Meal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Map;
 
 @org.springframework.stereotype.Repository
 public class MealRepo implements Repository<Meal> {
@@ -8,19 +20,42 @@ public class MealRepo implements Repository<Meal> {
     private static final String selectQuery = "SELECT id, mealName, mealCost, mealAvailable from meal";
     private static final String updateQuery = "UPDATE meal SET mealName = ?, mealCost = ?, mealAvailable = ? where id = ?";
     private static final String deleteQuery = "DELETE FROM meal where id = ?";
+    @Autowired
+    JdbcTemplate template;
+    @Autowired
+    JdbcOperations jdbcOperations;
 
     @Override
     public void create(Meal entity) {
-
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(template);
+        long id = insert
+                .withTableName("Meal")
+                .usingColumns("mealName", "mealCost", "mealAvailable")
+                .usingGeneratedKeyColumns("id")
+                .executeAndReturnKeyHolder(Map.of(
+                        "mealName", entity.getMealName(),
+                        "mealCost", entity.getMealCost(),
+                        "mealAvailable", entity.isMealAvailable())).getKey().longValue();
+        entity.setId(id);
     }
 
     @Override
     public Meal[] readAll() {
-        return new Meal[0];
+        ArrayList<Meal> values = new ArrayList<>();
+        SqlRowSet rowSet = jdbcOperations.queryForRowSet(selectQuery);
+        while (rowSet.next()) {
+            values.add(new Meal(
+                    rowSet.getLong("id"),
+                    rowSet.getString("mealName"),
+                    rowSet.getInt("mealCost"),
+                    rowSet.getBoolean("mealAvailable")));
+        }
+        return values.toArray(new Meal[0]);
     }
 
     @Override
     public Meal read(long id) {
+
         return null;
     }
 
