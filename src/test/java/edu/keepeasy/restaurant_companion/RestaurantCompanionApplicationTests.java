707bee064369c205a3e4e4985fdd949222c313b7
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-15 13:12:54 +0300

    edit: added mealRepo.save test

--- src/test/java/edu/keepeasy/restaurant_companion/RestaurantCompanionApplicationTests.java
+++ src/test/java/edu/keepeasy/restaurant_companion/RestaurantCompanionApplicationTests.java
@@ -1,13 +1,18 @@
 package edu.keepeasy.restaurant_companion;
 
+import edu.keepeasy.restaurant_companion.domain.Meal;
+import edu.keepeasy.restaurant_companion.repository.MealRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantCompanionApplicationTests {
-
+    @Autowired
+    MealRepo mealRepo;
     @Test
-    void contextLoads() {
+    void mealAdding(){
+        mealRepo.save(new Meal(0, "Шашлык", 150, true));
     }
 
 }
