08a436834b63293c453d1d016d26c144a6b0d34c
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-16 19:12:57 +0300

    add: OrderController, OrderItemController and UserController wirh all methods
    Took 34 minutes

---
+++ src/main/java/edu/keepeasy/restaurant_companion/controller/OrderController.java
@@ -0,0 +1,43 @@
+package edu.keepeasy.restaurant_companion.controller;
+
+import edu.keepeasy.restaurant_companion.repository.OrderRepo;
+import edu.keepeasy.restaurant_companion.resource.OrderResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    OrderRepo repo;
+
+    @PostMapping(value = "")
+    OrderResource post(@RequestBody OrderResource resource) {
+        return new OrderResource(repo.create(resource.toEntity()));
+    }
+
+    @GetMapping(value = "")
+    OrderResource[] getAll() {
+        return Arrays.stream(repo.readAll())
+                .map(OrderResource::new)
+                .toArray(OrderResource[]::new);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    OrderResource get(@PathVariable long id) {
+        return new OrderResource(repo.read(id));
+    }
+
+    @PutMapping(value = "/{id}")
+    OrderResource put(@PathVariable long id, @RequestBody OrderResource resource) {
+        return new OrderResource(repo.update(id, resource.toEntity()));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    OrderResource delete(@PathVariable long id) {
+        return new OrderResource(repo.delete(repo.read(id)));
+    }
+}
---
+++ src/main/java/edu/keepeasy/restaurant_companion/controller/OrderItemController.java
@@ -0,0 +1,44 @@
+package edu.keepeasy.restaurant_companion.controller;
+
+import edu.keepeasy.restaurant_companion.repository.OrderItemRepo;
+import edu.keepeasy.restaurant_companion.resource.OrderItemResource;
+import edu.keepeasy.restaurant_companion.resource.OrderResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/orderItem")
+public class OrderItemController {
+    @Autowired
+    OrderItemRepo repo;
+
+    @PostMapping(value = "")
+    OrderItemResource post(@RequestBody OrderItemResource resource) {
+        return new OrderItemResource(repo.create(resource.toEntity()));
+    }
+
+    @GetMapping(value = "")
+    OrderItemResource[] getAll(@RequestBody OrderResource resource) {
+        return Arrays.stream(repo.readAll(resource.toEntity()))
+                .map(OrderItemResource::new)
+                .toArray(OrderItemResource[]::new);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    OrderItemResource get(@PathVariable long id) {
+        return new OrderItemResource(repo.read(id));
+    }
+
+    @PutMapping(value = "/{id}")
+    OrderItemResource put(@PathVariable long id, @RequestBody OrderItemResource resource) {
+        return new OrderItemResource(repo.update(id, resource.toEntity()));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    OrderItemResource delete(@PathVariable long id) {
+        return new OrderItemResource(repo.delete(repo.read(id)));
+    }
+}
