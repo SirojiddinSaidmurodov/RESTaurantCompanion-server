8e12f96ba8463276fa52e448496eee0a065f11ce
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-16 15:20:03 +0300

    edit: migrated to JdbcTemplate for executing insertion
    Took 40 minutes

--- src/main/java/edu/keepeasy/restaurant_companion/repository/OrderRepo.java
+++ src/main/java/edu/keepeasy/restaurant_companion/repository/OrderRepo.java
@@ -3,36 +3,39 @@ package edu.keepeasy.restaurant_companion.repository;
 import edu.keepeasy.restaurant_companion.domain.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.sql.Types;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
 
 @org.springframework.stereotype.Repository
 public class OrderRepo implements Repository<Order> {
-    private static final String insertQuery = "INSERT INTO orders(waiterID, tableID, ready) value (?,?,?)";
     private static final String selectAllQuery = "SELECT id, waiterID, tableID, ready FROM orders";
     private static final String selectByIDQuery = "SELECT id, waiterID, tableID, ready FROM orders where id=?";
     private static final String updateQuery = "UPDATE orders SET id=?, waiterID=?, tableID=?, ready=? where id=?";
     private static final String deleteQuery = "DELETE FROM orders where id=?";
     @Autowired
     private JdbcOperations jdbcOperations;
+    @Autowired
+    private JdbcTemplate template;
 
     @Override
     public Order create(Order entity) {
-        Object[] args = new Object[]{
-                entity.getWaiterID(),
-                entity.getTableID(),
-                entity.isReady()};
-        int[] argTypes = new int[]{
-                Types.BIGINT,
-                Types.INTEGER,
-                Types.BOOLEAN};
-        return getResult(
-                jdbcOperations.queryForRowSet(
-                        insertQuery,
-                        args,
-                        argTypes));
+        long id = Objects.requireNonNull(new SimpleJdbcInsert(template)
+                .withTableName("orders")
+                .usingColumns("waiterID", "tableID", "ready")
+                .usingGeneratedKeyColumns("id")
+                .executeAndReturnKeyHolder(Map.of(
+                        "waiterID", entity.getWaiterID(),
+                        "tableID", entity.getTableID(),
+                        "ready", entity.isReady()))
+                .getKey()).longValue();
+        entity.setId(id);
+        return entity;
     }
 
     @Override
--- src/test/java/edu/keepeasy/restaurant_companion/RestaurantCompanionApplicationTests.java
+++ src/test/java/edu/keepeasy/restaurant_companion/RestaurantCompanionApplicationTests.java
@@ -1,7 +1,9 @@
 package edu.keepeasy.restaurant_companion;
 
 import edu.keepeasy.restaurant_companion.domain.Meal;
+import edu.keepeasy.restaurant_companion.domain.User;
 import edu.keepeasy.restaurant_companion.repository.MealRepo;
+import edu.keepeasy.restaurant_companion.repository.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,30 +14,24 @@ import static org.springframework.test.util.AssertionErrors.assertEquals;
 class RestaurantCompanionApplicationTests {
     @Autowired
     MealRepo mealRepo;
+    @Autowired
+    private UserRepo userRepo;
 
     @Test
     void mealAdding() {
         Meal meal = new Meal(0, "Шашлык", 150, true);
         meal = mealRepo.create(meal);
-        assertEquals("mealID", 1, meal.id);
+        assertEquals("mealID", 1, meal.getId());
         assertEquals("mealName", "Шашлык", meal.getMealName());
         assertEquals("mealCost", 150, meal.getClass());
         assertEquals("mealAvailable", true, meal.isMealAvailable());
     }
 
     @Test
-    void mealsReading() {
-        Meal[] meals = mealRepo.readAll();
-        for (Meal meal :
-                meals) {
-            System.out.println(meal.getMealCost());
-        }
-    }
-
-    @Test
-    void mealReading() {
-        Meal meal = mealRepo.read(1);
-        System.out.println(meal.getMealName());
+    void userAdding(){
+        User user = new User(0, "Sirojiddin", "xakep13", "pass", User.UserType.ADMIN);
+        user = userRepo.create(user);
+        assertEquals("uerID", 1, user.getId());
     }
 
 }
