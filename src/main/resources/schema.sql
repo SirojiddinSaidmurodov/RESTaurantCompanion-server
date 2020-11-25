cfa1661ab5fa1acfffd838cfc908cf1b8532a297
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-25 09:42:29 +0300

    edit: added some default values
    Took 3 minutes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `orders`
     `id`       SERIAL,
     `waiterID` BIGINT UNSIGNED NOT NULL,
     `tableID`  INT,
-    `ready`    BOOLEAN,
+    `ready`    BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`waiterID`) REFERENCES `user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -23,14 +23,14 @@ CREATE TABLE IF NOT EXISTS meal
     `id`            SERIAL PRIMARY KEY,
     `mealName`      CHAR(50),
     `mealCost`      INT,
-    `mealAvailable` BOOLEAN
+    `mealAvailable` BOOLEAN DEFAULT FALSE
 );
 CREATE TABLE IF NOT EXISTS `orderItems`
 (
     `id`       SERIAL,
     `mealID`   BIGINT UNSIGNED NOT NULL,
     `orderID`  BIGINT UNSIGNED NOT NULL,
-    `quantity` TINYINT,
+    `quantity` TINYINT DEFAULT 1,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`mealID`) REFERENCES `meal` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (`orderID`) REFERENCES `orders` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
